97dd40bd5522245e8ce581d49feb06cdaef05efe
Stephen Marden <stephen.j.marden@gmail.com>
2018-05-24 16:38:27 -0400

    todo-checkpoint-3-assignment: completed

--- src/App.js
+++ src/App.js
@@ -34,12 +34,16 @@ class App extends Component {
         this.setState({newTodoDescription: e.target.value});
     }
 
+    deleteTodo(index) {
+        this.setState({todos: [...this.state.todos.filter(element => element !== this.state.todos[index])]});
+    }
+
     render() {
         return (
             <div className="App">
                 <ul>
                     {this.state.todos.map( (todo, index) =>
-                        <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={() => this.toggleComplete(index)}/>
+                        <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={() => this.toggleComplete(index)} deleteTodo={() => this.deleteTodo(index)}/>
                     )}
                 </ul>
 
